82b241ef1d08ee40d5ad914ac7f63e634efa48a9
Jade Doiaz <jadedoiaz@gmail.com>
2025-05-08 22:22:28 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SolanaWalletWrapper } from '@/components/SolanaWalletWrapper'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter
+} from '@solana/wallet-adapter-wallets'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
 
 export default function App({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Mainnet
+  const endpoint = 'https://api.mainnet-beta.solana.com'
+  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()]
+
   return (
-    <SolanaWalletWrapper>
-      <Component {...pageProps} />
-    </SolanaWalletWrapper>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
