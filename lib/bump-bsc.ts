9ced005a2c63ae68b12ff8a360a0be702fef9a6a
Jade Doiaz <jadedoiaz@gmail.com>
2025-05-08 21:43:00 -0500

    Update bump-bsc.ts

--- lib/bump-bsc.ts
+++ lib/bump-bsc.ts
@@ -1,11 +1,11 @@
-import { ethers } from 'ethers'
+import { ethers, parseEther } from 'ethers'
 
 export async function sendMockBscBump(signer: ethers.Signer) {
   if (!signer) throw new Error('No signer available')
 
   const tx = {
     to: await signer.getAddress(),
-    value: ethers.utils.parseEther('0.00001') // self-send
+    value: parseEther('0.00001') // self-send
   }
 
   const txResp = await signer.sendTransaction(tx)
